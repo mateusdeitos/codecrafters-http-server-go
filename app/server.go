a0165c90235cac46e6c6cf1e12257651f636690e
mateusdeitos <mate.deitos@hotmail.com>
2024-09-10 20:30:31 -0300

    feat: testing the request path

--- app/server.go
+++ app/server.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"regexp"
 )
 
 // Ensures gofmt doesn't remove the "net" and "os" imports above (feel free to remove this!)
@@ -26,6 +27,31 @@ func main() {
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
+	}
+
+	buf := make([]byte, 1024)
+	n, err := conn.Read(buf)
+	if err != nil {
+		fmt.Println("Error reading: ", err.Error())
+		os.Exit(1)
+	}
+	fmt.Println(string(buf[:n]))
+
+	// extract the request target
+	r := regexp.MustCompile("[GET|POST|PUT|DELETE|PATCH|HEAD|OPTIONS] (.*) HTTP/1.1\r\n")
+	matches := r.FindAllStringSubmatch(string(buf[:n]), 1)
+	if len(matches) == 0 {
+		conn.Write([]byte("HTTP/1.1 400 Bad Request\r\n\r\n"))
+		conn.Close()
+		return
+	}
+
+	path := matches[0][1]
+	fmt.Printf("path: %s\n", path)
+	if path != "/" {
+		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+		conn.Close()
+		return
 	}
 
 	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
